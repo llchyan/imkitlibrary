725ad08aa0e44dc24009e9658951a415f3f37149
llchyan <haku_kuon@sina.com>
2016-01-09 09:14:41 +0800

    ok

---
+++ app/src/main/java/com/llchyan/imkitlibrary/MainActivity.java
@@ -0,0 +1,14 @@
+package com.llchyan.imkitlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
